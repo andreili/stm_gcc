65886668216c335db7711f233caf5dc42d8d9a7e
andreil <andreil499@gmail.com>
2016-10-15 23:11:34 +0300

    Добавлены функции для работы с памятью

--- include/my_func.h
+++ include/my_func.h
@@ -3,5 +3,7 @@
 
 int strlen(const char* str);
 void memcpy(char* dst, const char* src, int len);
+void memset(char* dst,char val, int len);
+int memcmp(const char *s1, const char *s2, int len);
 
 #endif //__MY_FUNC__
--- src/my_func.cpp
+++ src/my_func.cpp
@@ -13,3 +13,23 @@ void memcpy(char* dst, const char* src, int len)
 	while (len--)
 		*dst++ = *src++;
 }
+
+void memset(char* dst,char val, int len)
+{
+	while (len--)
+		*dst++ = val;
+}
+
+int memcmp(const char *s1, const char *s2, int len)
+{
+	int d = 0;
+
+	while (len--)
+	{
+		d = (int)*s1++ - (int)*s2++;
+		if (d)
+			break;
+	}
+
+	return d;
+}
